b31c09c13d8f5728151c5591abed674b35143366
sornichou <hulsensorn@gmail.com>
2020-07-30 00:04:09 +0200

    Adding Floating buttons and floating addTransaction widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:expenses_manager/widgets/newTransaction.dart';
+import 'package:expenses_manager/widgets/transactionList.dart';
 import 'package:flutter/material.dart';
 
-import 'widgets/userTransactions.dart';
+import 'widgets/transactionList.dart';
+import 'models/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,17 +20,88 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  // String titleInput;
-  // String amountInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't01',
+      title: 'New shoes',
+      amount: 59.95,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't02',
+      title: 'Fritness',
+      amount: 16.45,
+      date: DateTime.now().add(Duration(days: -2)),
+    ),
+    Transaction(
+      id: 't03',
+      title: 'Fuel',
+      amount: 45.09,
+      date: DateTime.now().add(Duration(days: -4)),
+    ),
+    Transaction(
+      id: 't04',
+      title: 'Mangas',
+      amount: 120.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't05',
+      title: 'New Phone',
+      amount: 499.99,
+      date: DateTime.now().add(Duration(days: -2)),
+    ),
+    Transaction(
+      id: 't06',
+      title: 'Sockets',
+      amount: 00.99,
+      date: DateTime.now().add(Duration(days: -4)),
+    ),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTransaction = Transaction(
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  void startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (buildContext) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Expenses manager'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => startAddNewTransaction(context),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -43,9 +117,14 @@ class MyHomePage extends StatelessWidget {
                 margin: EdgeInsets.all(25),
               ),
             ),
-            UserTransactions(),
+            TransactionList(_transactions),
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => startAddNewTransaction(context),
       ),
     );
   }
